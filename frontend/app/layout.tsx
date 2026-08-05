56e227266576c4c0ccf68e350e33891bc5471d57
Rohit Saundalkar <soundrohit632@gmail.com>
2026-08-05 17:55:24 +0530

    added meta pixels script

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -93,8 +93,33 @@ export default function RootLayout({
 })(window, document, "clarity", "script", "w8ygd5tzgk");`,
           }}
         />
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '927597176298283');
+fbq('track', 'PageView');`,
+          }}
+        />
       </head>
       <body className={`${inter.variable} font-sans antialiased`}>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            alt=""
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=927597176298283&ev=PageView&noscript=1"
+          />
+        </noscript>
         <ToastContainer
           position="top-right"
           autoClose={3000}
